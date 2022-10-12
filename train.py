4a80bd47962fff16995f09666da37bb5a23b717c
Aman Saraf <amansaraf99@gmail.com>
2022-10-12 10:47:18 -0700

    Run 10_10 Configuration

--- train.py
+++ train.py
@@ -98,6 +98,7 @@ def train(hyp, opt, device, tb_writer=None):
     train_path = data_dict['train']
     test_path = data_dict['val']
 
+
     # Freeze
     freeze = [f'model.{x}.' for x in (freeze if len(freeze) > 1 else range(freeze[0]))]  # parameter names to freeze (full or partial)
     for k, v in model.named_parameters():
